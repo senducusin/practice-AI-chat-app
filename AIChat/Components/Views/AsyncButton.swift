871a1c1851647a33321cbd7d1a880f54afd4ff68
Jansen Ducusin <senducusin@gmail.com>
2025-12-15 09:43:10 +0800

    [Feature] Add CreateAvatarView

---
+++ AIChat/Components/Views/AsyncButton.swift
@@ -0,0 +1,18 @@
+//
+//  AsyncButton.swift
+//  AIChat
+//
+//  Created by Jansen Ducusin on 12/15/25.
+//
+
+import SwiftUI
+
+struct AsyncButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AsyncButton()
+}
---
+++ AIChat/Core/CreateAvatar/CreateAvatarView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateAvatarView.swift
+//  AIChat
+//
+//  Created by Jansen Ducusin on 12/15/25.
+//
+
+import SwiftUI
+
+struct CreateAvatarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateAvatarView()
+}
