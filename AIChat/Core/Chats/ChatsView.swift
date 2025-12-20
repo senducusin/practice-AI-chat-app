10156b80f469e75c76785c0b16e3559c0a5cb700
Jansen Ducusin <senducusin@gmail.com>
2025-12-20 11:59:21 +0800

    [Feature] Add RecentAvatars

--- AIChat/Core/Chats/ChatsView.swift
+++ AIChat/Core/Chats/ChatsView.swift
@@ -9,12 +9,34 @@ import SwiftUI
 
 struct ChatsView: View {
     @State private var chats: [ChatModel] = ChatModel.mocks
-
+    @State private var recentAvatars: [AvatarModel] = AvatarModel.mocks
     @State private var path: [NavigationPathOption] = []
 
     var body: some View {
         NavigationStack(path: $path) {
             List {
+                if !recentAvatars.isEmpty {
+                    recentsSection
+                }
+
+                chatsSection
+            }
+            .navigationTitle("Chats")
+            .navigationDestinationForCoreModule(path: $path)
+        }
+    }
+
+    private var chatsSection: some View {
+        Section {
+            if chats.isEmpty {
+                Text("No chats found")
+                    .foregroundStyle(.secondary)
+                    .font(.title3)
+                    .frame(maxWidth: .infinity)
+                    .multilineTextAlignment(.center)
+                    .padding(40)
+                    .removeListRowFormatting()
+            } else {
                 ForEach(chats) { chat in
                     ChatRowCellViewBuilder(
                         currentUserId: nil,
@@ -32,13 +54,49 @@ struct ChatsView: View {
 
                 }
             }
-            .navigationTitle("Chats")
-            .navigationDestinationForCoreModule(path: $path)
+        } header: {
+            Text("Chats")
+        }
+    }
+
+    private var recentsSection: some View {
+        Section {
+            ScrollView(.horizontal) {
+                LazyHStack(spacing: 8) {
+                    ForEach(recentAvatars, id: \.self) { avatar in
+                        if let profileImageName = avatar.profileImageName {
+                            VStack(spacing: 8) {
+                                ImageLoaderView(urlString: profileImageName)
+                                    .aspectRatio(1, contentMode: .fit)
+                                    .clipShape(Circle())
+
+                                Text(avatar.name ?? "")
+                                    .font(.caption)
+                                    .foregroundStyle(.secondary)
+                            }
+                            .customButton {
+                                onAvatarPressed(avatar: avatar)
+                            }
+                        }
+                    }
+                }
+                .padding(.top, 12)
+            }
+            .frame(height: 120)
+            .scrollIndicators(.hidden)
+            .removeListRowFormatting()
+        } header: {
+            Text("Recents")
         }
     }
 
     private func onChatPressed(chat: ChatModel) {
         let pathModel = NavigationPathOption.chat(avatarId: chat.avatarId)
+        path.append(pathModel)
+    }
+
+    private func onAvatarPressed(avatar: AvatarModel) {
+        let pathModel = NavigationPathOption.chat(avatarId: avatar.avatarId)
         path.append(pathModel)
     }
 }
